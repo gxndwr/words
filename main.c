b7ef92adbed6b91f48a65670df127d83e8c67741
Ran Wang <gxndwr@outlook.com>
2020-07-23 21:27:09 +0800

    add choice test mode
    Signed-off-by: Ran Wang <gxndwr@outlook.com>

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 
 //#define DEBUG
 //#define MANUAL_CHECK
+#define  CHOICE_QUESTION
 
 #if defined(DEBUG)
 	#define dbg printf
@@ -133,10 +134,13 @@ int main(void)
 
     // display word one by one
     correct_num = 0;
+#ifndef   CHOICE_QUESTION
     disable_io_buffer();
+#endif
     for (i = 0; checked_num < exam.num; i++) {
         int index = rand()%exam.num;
         if (!(exam.word[index].flag & CHECKED)) {
+#ifndef CHOICE_QUESTION
                 printf("%s: ",exam.word[index].question);
                 answer = getchar();
 #ifndef MANUAL_CHECK
@@ -145,12 +149,113 @@ int main(void)
 
                 printf("%s\n",exam.word[index].answer);
 #endif
+#else
+                int answer_pos = rand()%4;
+                int opt_index;
+                dbg("answer_pos: %d\n", answer_pos);
+                // Print question
+                printf("\n%s:\n\n", exam.word[index].question);
+
+                int used_candidate_index_1;
+                int used_candidate_index_2;
+                int used_candidate_index_3;
+                // Print 1st option
+                if (0 == answer_pos) {
+                    printf("A) %s    ", exam.word[index].answer);
+                } else {
+                    opt_index = rand()%exam.num;
+
+                    while(1) {
+                        if (opt_index != index)
+                            break;
+                        opt_index = rand()%exam.num;
+                    }
+
+                    printf("A) %s    ", exam.word[opt_index].answer);
+                    used_candidate_index_1 = opt_index;
+                }
+                // Print 2nd option
+                if (1 == answer_pos) {
+                    printf("B) %s    ", exam.word[index].answer);
+                } else {
+                    opt_index = rand()%exam.num;
+
+                    while(1) {
+                        if (opt_index != index)
+                            if (opt_index != used_candidate_index_1)
+                                break;
+                        opt_index = rand()%exam.num;
+                    }
+
+                    printf("B) %s    ", exam.word[opt_index].answer);
+                    used_candidate_index_2 = opt_index;
+                }
+                // Print 3rd option
+                if (2 == answer_pos) {
+                    printf("C) %s    ", exam.word[index].answer);
+                } else {
+                    opt_index = rand()%exam.num;
+
+                    while(1) {
+                        if (opt_index != index)
+                            if (opt_index != used_candidate_index_1)
+                                if (opt_index != used_candidate_index_2)
+                                    break;
+                        opt_index = rand()%exam.num;
+                    }
+
+                    printf("C) %s    ", exam.word[opt_index].answer);
+                    used_candidate_index_3 = opt_index;
+                }
+                // Print 4th option
+                if (3 == answer_pos) {
+                    printf("D) %s\n", exam.word[index].answer);
+                } else {
+                    opt_index = rand()%exam.num;
+
+                    while(1) {
+                        if (opt_index != index)
+                            if (opt_index != used_candidate_index_1)
+                                if (opt_index != used_candidate_index_2)
+                                    if (opt_index != used_candidate_index_3)
+                                    break;
+                        opt_index = rand()%exam.num;
+                    }
+
+                    printf("D) %s\n", exam.word[opt_index].answer);
+                }
+
+                //printf("\n");
+
+                int a;
+                a = getchar() - 'a';
+                if (a == answer_pos) {
+                    printf("Correct!\n");
+                    correct_num++;
+                } else {
+                    printf("Wrong... The answer is %s\n", exam.word[index].answer);
+                    int original_sec, current_sec;
+                    clock_gettime(CLOCK_REALTIME, tp);
+                    original_sec = tp->tv_sec;
+                    while(1){
+                    clock_gettime(CLOCK_REALTIME, tp);
+                    current_sec = tp->tv_sec;
+                        if (current_sec > original_sec + 4)
+                            break;
+                    }
+                }
+
+                //printf("%s\n",exam.word[index].answer);
+                while(getchar() != '\n'){};
+#endif
                 exam.word[index].flag |= CHECKED;
                 checked_num++;
                 //dbg("exam.word[%02d]: %s \t checked_num: %d\n", index, exam.word[index].question, checked_num);
         }
     }
+#ifndef   CHOICE_QUESTION
     enable_io_buffer();
+#endif
 
     printf("--------------\n");
     printf("Total: %d\n", exam.num);
